328f944fd68cee02a0b34c8f3aa8e9408a8c9ebb
MoRunChang <709403987ac@gmail.com>
2017-02-09 11:44:32 +0800

    upload code

---
+++ C++ Primer/Chapter 4/code/4.2/negativeNumber.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "21 % 6 = " << 21 % 6 << endl; // 3
+    cout << "21 % 7 = " << 21 % 7 << endl; // 0
+    cout << "-21 % -8 = " << -21 % -8 << endl; // -5
+    cout << "21 % -5 = " << 21 % -5 << endl; // 1
+
+    cout << "21 / 6 = " << 21 / 6 << endl; // 3
+    cout << "21 / 7 = " << 21 / 7 << endl; // 3
+    cout << "-21 / -8 = " << -21 / -8 << endl; // 2
+    cout << "21 / -5 = " << 21 / -5 << endl;  // -4
+    return 0;
+}
